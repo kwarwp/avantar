62dd7f2f9bfd0ec2e37875a2a5ad65a560a68ffc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-18 12:20:57 -0300

    Saved root/main.py @2023-10-18 15:20

--- root/main.py
+++ root/main.py
@@ -1,4 +1,6 @@
 # avantar.root.main.py
 [mapa_ROOT.Jc]
 local=[0,0]
-tit="Uma aventura!", texto="Missão Nova", foi=".i.RO1zz0zz1.0.0"}
+tit="Uma aventura!"
+texto="Missão Nova"
+foi=".i.RO1zz0zz1.0.0"}

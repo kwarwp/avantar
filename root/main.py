370585cc8012a2910b44cc35f61337d0563ced7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-18 16:07:01 -0300

    Saved root/main.py @2023-10-18 19:07

--- root/main.py
+++ root/main.py
@@ -4,3 +4,28 @@ local=[0,0]
 tit="Uma aventura!"
 texto="Missão Nova"
 foi=".i.RO1zz0zz1.0.0"
+[mapa0_ROOT]
+Jb = {local=[0,0], tit="Fuja do Tissunami!", texto="Missão Climática", foi=".i.RO1zz0zz1.0.0"}
+Xb = {local=[0,0], img=".i.Missao._AVENTURA.1", x=500, tit="Explorar!", texto="Missão de Exploração", foi=".i.BIOzz0zz1.0.0"}
+Xe = {local=[0,0], x=900, tit="Explorando a Antártida!", texto="Missão de Exploração Antártica", foi=".i.actbio"}
+Ep = {local=[0,1], img=".i.Missao._MAYSA.0", y=400, tit="Busca por suprimentos!", texto="Buscas suprmentos", foi=".i.pint"}
+
+[mapa1_ROOT]
+Ss = {local=[0,1], x=50, tit="Suprimentos!", texto="Missão de Suprimentos", foi=".i.YELzz0zz1.0.0"}
+Cs = {local=[0,1], x=400, tit="Busca por caixas perdidas!", texto="Buscas por caixas", foi=".i.caix"}
+Es = {local=[0,1], x=800, tit="Encontro com outros exploradores!", texto="Buscas por exploradores", foi=".i.RO1zz7zz1.0.0"}
+#Sc = {local=[0,1], x=1100, tit="Busca por suprimentos!", texto="Buscas suprmentos", foi=".i.pint"}
+
+[mapa2_ROOT]
+
+Bc = {local=[0,2], tit="Bioma", texto="Bioma", foi=".i.pint"}
+Ac = {local=[0,2], tit="Atmosfera", texto="Atmosfera", foi=".i.pint"}
+Cc = {local=[0,2], tit="Clima", texto="Clima", foi=".i.pint"}
+Kc = {local=[0,2], tit="Kaique", texto="Kaique", foi=".i.pint"}
+
+[mapa3_ROOT]
+Fc = {local=[0,3], tit="Criosfera", texto="Criosfera", foi=".i.pint"}
+Gc = {local=[0,3], tit="Geodinâmica", texto="Geodinâmica", foi=".i.pint"}
+Pc = {local=[0,3], tit="Perigo", texto="Perigo", foi=".i.pint"}
+Dc = {local=[0,3], tit="Desastre", texto="Desastre", foi=".i.pint"}
+#Ep = {local=[0,1], img="https://imgur.com/sxAm5LA.png", y=400, tit="Busca por suprimentos!", texto="Buscas suprmentos", foi=".i.pint"}

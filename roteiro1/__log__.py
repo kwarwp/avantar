4c94aae2a56d5d077a00cfb92a41b4ea00b07210
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2023-10-19 09:30:35 -0300

    Created roteiro1/__log__.py @2023-10-19 12:30

---
+++ roteiro1/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Oct 19 2023 09:30:33.835 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  p_bote = {img=".i.PUZZLE.AdvBo0.4.4", pr=1, foi=".i.ROOTzz0.0.0" , texto = "Conseguiu embarcar no bote!", cena=".i.CN._RO1.4", dim=[4,4]}
+                                                                                                                                           ^
+SyntaxError: invalid syntax
+'''},
